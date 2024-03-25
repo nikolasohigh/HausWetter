8d13af49be147bf884bd52f2d331275ee55d2646
nikolasohigh <kolya.bondar.yt@gmail.com>
2024-03-26 00:10:08 +0100

    naxui weather

--- src/js/elements.js
+++ src/js/elements.js
@@ -1,40 +1,21 @@
 const wrapper = document.getElementById('main');
-const date = new Date();
 
-let userPosition = {
-  isDownloaded: false,
-  locationKey: null,
-  city: null,
-  longitude: null,
-  latitude: null
+let loginData = {
+  isRemembered: false,
+  api: null,
+  channel: null
 }
 
 let options = {
-  isDownloaded: false,
-  urlMQTT: 'https://corsproxy.io/?https://dev.rightech.io/api/v1/objects/mqtt-kolya_bondar_yt-3f68k5',
-  apiKeyMQTT: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2NTg1MzFlOTk2YmE1ZjY2ZTBhNDk0NTAiLCJzdWIiOiI2NTgwY2YzYmE2OWI2MTJmNmE1YjUzNGIiLCJncnAiOiI2NTgwY2YzYmE2OWI2MTJmNmE1YjUzNGEiLCJvcmciOiI2NTgwY2YzYmE2OWI2MTJmNmE1YjUzNGEiLCJsaWMiOiI1ZDNiNWZmMDBhMGE3ZjMwYjY5NWFmZTMiLCJ1c2ciOiJhcGkiLCJmdWxsIjpmYWxzZSwicmlnaHRzIjoxLjUsImlhdCI6MTcwMzIyNzg4MSwiZXhwIjoyMjEwNzEzMjAwfQ.TwTeKWAZtS9LEvo0N8ESuh6AXeoVayiWLG6QmGnXhVM',
-  apiKeyWeather: 'GCS3qGbShGtrGN3opAK1SfaIdob7awdI',
+  adminTelegramBotAPI: '6760658426:AAED14SgTV-MxbCDgX1k_CmEAauSElphxqI',
+  adminTelegramChatID: '6632566922'
 }
 
 const espValues = {
-  status: false,
   temperature: 0,
   humidity: 0,
   pressure: 0,
   ppm: 0
-}
-
-const currentState = {
-  ico: null,
-  state: null,
-  temperature: null
-}
-
-let hourlyForecast = [];
-let dailyForecast = [];
-
-function toCelcium(temperatureF) {
-  return Math.round((temperatureF-32)*(5/9));
 }
 
 function saveOptions(key, obj) {
@@ -42,3 +23,21 @@ function saveOptions(key, obj) {
   localStorage.setItem(`${key}`, JSON.stringify(obj));
 }
 
+function startApp() {
+  buildMonitoring();
+  buildStatistic();
+  initSwipeMenu();
+}
+
+
+function initSwipeMenu() {
+  $('.main').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: false,
+    swipe: true
+
+ });
+}
---
+++ src/js/statistic.js
@@ -0,0 +1,9 @@
+const statWrapper = document.createElement('div'),
+      testtttt = document.createElement('div');
+      testtttt.textContent = "sadfdfsasdf";
+
+      
+function buildStatistic() {
+    wrapper.appendChild(statWrapper);
+    statWrapper.appendChild(testtttt);
+}
